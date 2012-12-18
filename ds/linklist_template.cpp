32b1b74a428b37cbfe48f5ab1f809fe4ff5d4562
s4553711 <s4553711@gmail.com>
2012-12-18 23:59:45 +0800

    add example of operation

--- ds/linklist_template.cpp
+++ ds/linklist_template.cpp
@@ -1,22 +1,36 @@
 #include <iostream>
+using namespace std;
+
 template <class T>
 class ChainNode {
 	private:
+
+	public:
 		T data;
 		ChainNode<T> *link;
+
+		ChainNode(void){};
+		ChainNode(const T& data){ this->data = data; };
+		ChainNode(const T& data, ChainNode<T> *link){
+			this->data = data;
+			this->link = link;
+		};
 };
 
 template <class T>
 class Chain {
 	private:
 		ChainNode<T> *first;
+		int count;
 
 	public:
-		Chain(void){ first = NULL; };
+		Chain(void){ first = NULL; count = 0; };
 		bool isEmpty() const {return first == NULL;}
 		int IndexOf(const T&) const;
+		int size(void){return count;}
 		void Delete(int);
 		void Insert(int,const T&);
+		void Display(void);
 		~Chain();
 };
 
@@ -56,9 +70,11 @@ void Chain<T>::Delete(int theIndex){
 		deleteNode = first;
 		first = first->link;
 		delete deleteNode;
+		count--;
+
 	} else {
 		ChainNode<T> *p = first;
-		for(int i =0; i<= theIndex-1; i++){
+		for(int i =0; i< theIndex-1; i++){
 			if (p == NULL) throw "Element not exist";
 			p = p->link;
 		}
@@ -66,6 +82,7 @@ void Chain<T>::Delete(int theIndex){
 		deleteNode = p->link;
 		p->link = p->link->link;
 		delete deleteNode;
+		count--;
 	}
 }
 
@@ -75,6 +92,7 @@ void Chain<T>::Insert(int theIndex, const T& theElement){
 
 	if (theIndex == 0){
 		first = new ChainNode<T>(theElement,first);
+		count++;
 	} else {
 		ChainNode<T> *p = first;
 		for(int i = 0; i < theIndex-1; i++){
@@ -83,12 +101,40 @@ void Chain<T>::Insert(int theIndex, const T& theElement){
 		}
 		
 		p->link = new ChainNode<T>(theElement, p->link);
+		count++;
 	}
 };
 
-using namespace std;
-int main() {
-	cout << "THis is LinkList" << endl;
+template <class T>
+void Chain<T>::Display(void){
 
-	Chain<int> *linklist = new Chain<int>;
+	ChainNode<T> *p = first;
+
+	while(p != NULL){
+		cout << " <- " << p->data;
+		p = p->link;
+	}
+	cout << endl;
+};
+
+int main() {
+	cout << "This is LinkList .." << endl;
+
+	Chain<int> *list = new Chain<int>;
+	cout << "Size: " << list->size() << endl;
+
+	list->Insert(0,1);
+	list->Display();
+	list->Insert(0,2);
+	list->Display();
+	list->Insert(1,3);
+	list->Display();
+	list->Insert(1,4);
+	list->Display();
+	list->Insert(0,5);
+	list->Display();
+	list->Delete(2);
+	list->Display();
+
+	cout << "Size: " << list->size() << endl;
 }
